122534d7f672dcba4d58b2d0f1f7cbd4cee24858
Changlin Li <mail@changlinli.com>
2016-10-25 03:52:58 -0400

    Add Nix file
    Helps with shelling into this and building everything

---
+++ default.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, bifunctors, blaze-html, filepath
+      , hakyll, stdenv
+      }:
+      mkDerivation {
+        pname = "shuangrimu";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base bifunctors blaze-html filepath hakyll
+        ];
+        license = stdenv.lib.licenses.gpl3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
